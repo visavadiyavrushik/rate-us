d665d6c9642543feb3dfb9879a13526ba7257680
vrushik <180120116126@git.org.in>
2021-08-10 12:04:16 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const rating = document.querySelector('#rating');
+function rate(e) {
+    const stars = document.querySelectorAll('.star');
+
+    [...stars].map((stars) => stars.classList.remove("hovered"))
+    e.classList.add('hovered');
+
+    let points = ["Poor", "Fair", "Average", "Good", "Excellent"];
+    rating.innerHTML = points[e.getAttribute("data-value") - 1];
+    console.log(e.getAttribute("data-value"));
+
+}
